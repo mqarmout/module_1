984ac5e9acbd572ad42ba27ec815828ffedfa8d0
mqarmout <majedamf@gmail.com>
2024-05-12 12:30:41 -0400

    modifed the python code to include the clean function and corrected a bug where the file wasn't writing to the correct location

--- majed_qarmout_101565483.py
+++ majed_qarmout_101565483.py
@@ -13,7 +13,8 @@ def get_page():
 	global url
 	
 	# Code here - Ask the user to input "Enter url of a medium article: " and collect it in url
-	url = input("please provide the the URL of a medium article for scrapping: ")
+	# url = input("please provide the the URL of a medium article for scrapping: ")
+	url = "https://medium.com/@subashgandyer/papa-what-is-a-neural-network-c5e5cc427c7"
 	# Code ends here
 	
 	# handling possible error
@@ -35,33 +36,38 @@ def get_page():
 
 # function to remove all the html tags and replace some with specific strings
 def clean(text):
-    rep = {"<br>": "\n", "<br/>": "\n", "<li>":  "\n"}
+    rep = {"<br>": "\n", "<br/>": "\n", "<li>":  "\n",
+		   "Sign up\n": "", "Sign in\n": "", "Listen\n": "",
+		   "Share\n": "", "Follow\n": "", "Help\n": "",
+		   "Status\n": "", "About\n": "", "Careers\n": "",
+		   "Blog\n": "", "Privacy\n": "", "Blog\n": "",
+		   "Terms\n": "", "Text to speech\n": "", "Teams\n": ""}
     rep = dict((re.escape(k), v) for k, v in rep.items()) 
     pattern = re.compile("|".join(rep.keys()))
     text = pattern.sub(lambda m: rep[re.escape(m.group(0))], text)
     text = re.sub('\<(.*?)\>', '', text)
-    return text
+    return text.strip()
 
 
 def collect_text(soup):
 	text = f'url: {url}\n\n'
 	para_text = soup.find_all('p')
-	print(f"paragraphs text = \n {para_text}")
+	# print(f"paragraphs text = \n {para_text}")
 	for para in para_text:
 		text += f"{para.text}\n\n"
 	return text
 
 # function to save file in the current directory
 def save_file(text):
-	if not os.path.exists('./scraped_articles'):
-		os.mkdir('./scraped_articles')
+	# if not os.path.exists('./scraped_articles'):
+	# 	os.mkdir('./scraped_articles')
 	name = url.split("/")[-1]
 	print(name)
-	fname = f'scraped_articles/{name}.txt'
+	fname = f'{name}.txt'
 	
 	# Code here - write a file using with (2 lines)
 	with open(fname, "w") as file:
-		file.write(text)
+		file.write(clean(text))
 	# Code ends here
 
 	print(f'File saved in directory {fname}')
